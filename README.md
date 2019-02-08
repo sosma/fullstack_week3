8d218044fa8e21884a42278de12556d4658b6b78
osma <okovaskainen@evo.paivola.fi>
2019-02-08 16:33:28 +0200

    add index.js

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fullstack osa 3
+
+herokussa oleva sovellus [linkki](https://blooming-harbor-63158.herokuapp.com/api/persons)
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 //use tiny
 // app.use(morgan('tiny'))
