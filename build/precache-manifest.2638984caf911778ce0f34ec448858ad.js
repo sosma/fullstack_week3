b7c52532e850f5fd0f70a49ee7978a3abcf84531
osma <okovaskainen@evo.paivola.fi>
2019-02-08 16:31:29 +0200

    build

---
+++ build/precache-manifest.2638984caf911778ce0f34ec448858ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc2f839e1c54e4b13d71",
+    "url": "/static/js/main.dc2f839e.chunk.js"
+  },
+  {
+    "revision": "2f6f48e9ff4ae5f959c8",
+    "url": "/static/js/1.2f6f48e9.chunk.js"
+  },
+  {
+    "revision": "e85054f8418442db0c9f61eae44d2f7d",
+    "url": "/index.html"
+  }
+];
